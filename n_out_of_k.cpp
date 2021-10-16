f5cc50e5e73a81ec8848866670c95247f0241cfd
D R Raghuram <16043231+raghuramdr@users.noreply.github.com>
2021-10-16 20:28:38 +0200

    Create n_out_of_k.cpp
    A generalization of problem 2033 on Leetcode to handle an infinite stream of arrays where the number of elements repeated across the array is K.

---
+++ n_out_of_k.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    vector <int> populateResult(vector<int> &input, vector<int> &output,
+                                   map<int,int> &hashmap)
+    {   
+ 
+            map<int, bool> temp_map;
+            for(int idx=0; idx<input.size(); ++idx)
+            {
+                if(temp_map.find(input[idx]) == temp_map.end())
+                {
+                   temp_map[input[idx]] = true; 
+                    
+                  if(hashmap.find(input[idx]) == hashmap.end())
+                {
+                    hashmap[input[idx]] = 1;
+                }
+                else{
+                    hashmap[input[idx]]++;
+                    
+                    if(hashmap[input[idx]] == K)
+                    {
+                        output.push_back(input[idx]);
+                    }
+                   
+                }
+                }
+                
+               
+        }
+        return output;    
+    }
+       
+    //vector<vector<int>>& nums is an infinite stream of vectors containing integers
+    //K is the number of repeats expected across all of the arrays. 
+    //Repeat elements within the same array are ignored.
+    
+    vector<int> twoOutOfThree(vector<vector<int>>& nums, int K) {
+        
+        vector <int> result; 
+        map <int, int> hashmap;
+      
+        vector<vector<int>>::iterator it = nums.begin();
+        while(it != nums.end())
+        {
+          result = populateResult(*it, result, hashmap, K);
+          **it;
+        }
+        
+        return result;
+    }
+};
